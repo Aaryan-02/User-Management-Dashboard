40c3ce9d77ef9e3917b1ed86f6c4ed4f8376f364
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2025-01-29 18:49:37 +0530

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">User Management Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
